d248a40b73e9bb08b759fa213f91b247a01b1a5d
mudbungie <mudbungie@gmail.com>
2016-06-22 16:49:00 -0700

    minimum working model

--- sshinit.py
+++ sshinit.py
@@ -60,7 +60,7 @@ def createKey(user, host):
     subprocess.call(['ssh-keygen','-t','ed25519','-f',keypath,'-C',
         user+'@'+host,'-N',''])
     print('Keypair created in', keypath)
-    return True
+    return keypath
 
 # Checks the ~/.ssh/config file for existing configuration. Returns line range
 # or none.
@@ -73,43 +73,60 @@ def findHostLine(config, host):
 
 # Comment out parameters that we set.
 def commentUntilNextHost(config, start):
-    user = re.compile('^\s*user\s*\w{1,32}\s*$')
-    proxy = re.compile('^\s*ProxyCommand')
-    anyhostline = re.compile('^\s*Host')
-    for line in config[index:]:
+    user = re.compile(r'^\s*user\s*\w{1,32}\s*$')
+    proxy = re.compile(r'^\s*ProxyCommand.*')
+    idline = re.compile(r'^\s*IdentityFile.*')
+    anyhostline = re.compile(r'^\s*Host.*')
+
+    try:
+        config[start] = '#' + config[start]
+    except IndexError:
+        pass
+    for index, line in enumerate(config[start:]):
         if anyhostline.match(line):
+            print('Host match for:', line)
             return config
-        if user.match(line) or proxy.match(line):
-            line = '#' + line
+        elif user.match(line) or proxy.match(line) or idline.match(line):
+            print('Directive match for:', line)
+            config[index + start] = '#' + line
+        else:
+            print('No match for:', line)
     return config
 
 # Make the config file have a valid entry for this host.
-def updateConfig(host, user, bastion):
+def updateConfig(user, host, bastion):
     conffilename = environ['HOME'] + '/.ssh/config'
     try:
         with open(conffilename, 'r') as conffile:
-            config = conf.readlines()
+            config = conffile.readlines()
     except FileNotFoundError:
         config = []
 
+    start = findHostLine(config, host)
     config = commentUntilNextHost(config, start)
-
-    userline = '    user ' + user
+    
+    config.insert(start, '\n')
+    userline = '    user ' + user + '\n'
     config.insert(start, userline)
+    idline = '    IdentityFile ' + environ['HOME'] + '/.ssh/auto/' + user +\
+        '@' + host + '\n'
+    config.insert(start, idline)
     if bastion:
-        config.insert(start, '    ProxyCommand ssh ' + bastion +\
-            ' -W ' + host + '%p')
+        bastionline = '    ProxyCommand ssh ' + bastion + ' -W ' + host + '%p\n'
+        config.insert(start, bastionline)
+    hostline = 'Host ' + host + '\n'
+    config.insert(start, hostline)
+
     with open(conffilename, 'w') as conffile:
-        conffile.write(config)
+        conffile.writelines(config)
 
     return True
 
-def insertKey(host, user, bastion):
-    remoteCommand = "'mkdir .ssh 2> /dev/null; cat >> .ssh/authorized_keys'"
+def insertKey(user, host, bastion):
+    remoteCommand = 'mkdir .ssh 2> /dev/null; cat >> .ssh/authorized_keys'
     connstring = user+'@'+host
-    with open(environ['HOME'] + '/.ssh/auto/' + connstring + '.pub') as f:
-        pubkey = f.read()
-    subprocess.call('ssh', user+'@'+host, remotecommand, stdin=pubkey)
+    with open(environ['HOME'] + '/.ssh/auto/' + connstring + '.pub') as pubkey:
+        subprocess.call(['ssh', user+'@'+host, remoteCommand], stdin=pubkey)
     print('Key installed.')
 
 if __name__ == '__main__':
@@ -117,6 +134,8 @@ if __name__ == '__main__':
         user, host, root, bastion = handle_args(argv)
         print(user, host, root, bastion)
         createKey(user, host)
+        updateConfig(user, host, bastion)
+        insertKey(user, host, bastion)
     except InputError:
         print("usage: ssh-init [-r] TARGET [HOP]")
         
