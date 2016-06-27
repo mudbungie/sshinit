14f84a175b50fa1cbd0cabfbf7818456be289f00
mudbungie <mudbungie@gmail.com>
2016-06-26 23:15:18 -0700

    working, with bastion support

--- sshinit.py
+++ sshinit.py
@@ -18,6 +18,9 @@ import subprocess
 import re
 from os import environ, chmod, makedirs
 
+class InputError(Exception):
+    pass
+
 # Goes through the list of arguments passed, and returns the target system's
 # user, host, whether or not we're providing root access, and an intermediate
 # hop, if any.
@@ -26,39 +29,53 @@ def handle_args(argv):
     # lack an @ sign.
     if not 1 <= len(argv) <= 3:
        raise InputError('Argument list out of range.')
-    root = False
-    target = None
+    host = None
     bastion = None
+    user = None
+    root = False
+    print(argv)
+    # Skip first(this prog), then host, then bastion.
     for arg in argv[1:]:
         print(arg)
         if arg == '-r' or arg == '--root':
             root = True
-        elif '@' in arg:
-            if target:
-                bastion = arg
-            else:
-                user, host = arg.split('@')
-                target = True
         else:
-            raise InputError('Expected username@hostname')
+            if host:
+                # If you argued more than host and bastion, you're wrong.
+                if bastion:
+                    raise InputError('Too many arguments.')
+                else:
+                    bastion = arg
+            # Splitting on @, host is last, then user. Err if too many @
+            else:
+                target = arg.split('@')
+                host = target[-1]
+                if len(target) == 2:
+                    user = target[0]
+                elif len(target) > 2:
+                    raise InputError
+    if not user:
+        # We want to have a user defined. It's not technological requirement.
+        raise InputError
+
     return user, host, bastion, root
         
-class InputError(Exception):
-    pass
-
 # Makes an SSH key, and puts it into $HOME/.ssh/auto/[target]
 def createKey(user, host):
     keydir = environ['HOME'] + '/.ssh/auto'
     keypath = keydir + '/' + user + '@' + host
 
     # Make sure that we can insert keys...
-    try:
-        makedirs(keydir)
-        chmod(keydir, 0o700)
-    except FileExistsError:
-        pass
+    makedirs(keydir, exist_ok=True)
+    chmod(keydir, 0o700)
+
+    # Actually make it.
+    if user:
+        target = user + '@' + host
+    else:
+        target = host
     subprocess.call(['ssh-keygen','-t','ed25519','-f',keypath,'-C',
-        user+'@'+host,'-N',''])
+        target,'-N',''])
     print('Keypair created in', keypath)
     return keypath
 
@@ -71,26 +88,29 @@ def findHostLine(config, host):
             return index
     return len(config)
 
-# Comment out parameters that we set.
+# Comment out parameters that we set: user, proxy, identityfile, host.
 def commentUntilNextHost(config, start):
-    user = re.compile(r'^\s*user\s*\w{1,32}\s*$')
-    proxy = re.compile(r'^\s*ProxyCommand.*')
-    idline = re.compile(r'^\s*IdentityFile.*')
-    anyhostline = re.compile(r'^\s*Host.*')
+    user = re.compile(r'^\s*user\s.*$')
+    proxy = re.compile(r'^\s*ProxyCommand\s.*')
+    idline = re.compile(r'^\s*IdentityFile\s.*')
+    anyhostline = re.compile(r'^\s*Host\s.*')
 
     try:
         config[start] = '#' + config[start]
     except IndexError:
         pass
+    # Iterate over lines from start, comment things until you hit nest host. 
     for index, line in enumerate(config[start:]):
         if anyhostline.match(line):
             print('Host match for:', line)
+            print('Stopping ssh configuration.')
             return config
         elif user.match(line) or proxy.match(line) or idline.match(line):
-            print('Directive match for:', line)
+            print('Commenting out:', line)
             config[index + start] = '#' + line
         else:
-            print('No match for:', line)
+            print('Unmanaged line:', line)
+    # In case there is no next host config, just give the file back.
     return config
 
 # Make the config file have a valid entry for this host.
@@ -105,14 +125,15 @@ def updateConfig(user, host, bastion):
     start = findHostLine(config, host)
     config = commentUntilNextHost(config, start)
     
+    # Add the relevant lines for the config.
     config.insert(start, '\n')
-    userline = '    user ' + user + '\n'
-    config.insert(start, userline)
-    idline = '    IdentityFile ' + environ['HOME'] + '/.ssh/auto/' + user +\
-        '@' + host + '\n'
+    if user:
+        userline = '    user ' + user + '\n'
+        config.insert(start, userline)
+    idline = '    IdentityFile ' + createKey(user, host) + '\n'
     config.insert(start, idline)
     if bastion:
-        bastionline = '    ProxyCommand ssh ' + bastion + ' -W ' + host + '%p\n'
+        bastionline = '    ProxyCommand ssh ' + bastion + ' -W ' + host + ':%p\n'
         config.insert(start, bastionline)
     hostline = 'Host ' + host + '\n'
     config.insert(start, hostline)
@@ -126,14 +147,18 @@ def insertKey(user, host, bastion):
     remoteCommand = 'mkdir .ssh 2> /dev/null; cat >> .ssh/authorized_keys'
     connstring = user+'@'+host
     with open(environ['HOME'] + '/.ssh/auto/' + connstring + '.pub') as pubkey:
-        subprocess.call(['ssh', user+'@'+host, remoteCommand], stdin=pubkey)
+        if bastion:
+            subprocess.call(['ssh', user+'@'+host, remoteCommand], 
+                stdin=pubkey)
+        else:
+           pass 
     print('Key installed.')
 
 if __name__ == '__main__':
     try:
-        user, host, root, bastion = handle_args(argv)
-        print(user, host, root, bastion)
-        createKey(user, host)
+        user, host, bastion, root = handle_args(argv)
+        print(user, host, bastion, root)
+        #createKey(user, host) # Called in updateConfig
         updateConfig(user, host, bastion)
         insertKey(user, host, bastion)
     except InputError:
